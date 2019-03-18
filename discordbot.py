4d022bf8a3410489c8fac5b5fa2d5c7f78bd0d8a
94tiger <94tiger@naver.com>
2019-03-19 00:43:39 +0900

    건의함 기능 추가

--- discordbot.py
+++ discordbot.py
@@ -332,8 +332,9 @@ async def on_message_delete(message):
 # if author.id == '':
 #    await client.send_message(message.channel, "")
 
-f = open('./TOKEN.txt', 'r')
-TOKEN = f.read()
-f.close()
+# f = open('./TOKEN.txt', 'r')
+# TOKEN = f.read()
+# f.close()
 
-client.run(TOKEN)
+access_token = os.inviron("BOT_TOKEN")
+client.run(access_token)
